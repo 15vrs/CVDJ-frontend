18d7cacadc8165dca7d30dc73eea7dffadff40c9
vivian_so <vivianrachelso@gmail.com>
2020-11-17 18:14:35 -0500

    wip: webcam on by default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { WebcamModule } from 'ngx-webcam';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ConnectComponent } from './connect/connect.component';
 import { MainComponent } from './main/main.component';
 import { SongInfoComponent } from './song-info/song-info.component';
 import { EmotionModalComponent } from './emotion-modal/emotion-modal.component';
+import { CameraComponent } from './camera/camera.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +22,15 @@ import { EmotionModalComponent } from './emotion-modal/emotion-modal.component';
     ConnectComponent,
     MainComponent,
     SongInfoComponent,
-    EmotionModalComponent
+    EmotionModalComponent,
+    CameraComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
