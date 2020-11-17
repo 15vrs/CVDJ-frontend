45cfdc62aa4edf2695024e19da298201a6684048
vivian_so <vivianrachelso@gmail.com>
2020-11-17 18:14:35 -0500

    wip: webcam on by default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ConnectComponent } from './connect/connect.component';
 import { MainComponent } from './main/main.component';
 import { SongInfoComponent } from './song-info/song-info.component';
 import { EmotionModalComponent } from './emotion-modal/emotion-modal.component';
 import { MusicPlayerComponent } from './music-player/music-player.component';
+import { CameraComponent } from './camera/camera.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +23,15 @@ import { MusicPlayerComponent } from './music-player/music-player.component';
     MainComponent,
     SongInfoComponent,
     EmotionModalComponent,
-    MusicPlayerComponent
+    MusicPlayerComponent,
+    CameraComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/connect/connect.component.ts
+++ src/app/connect/connect.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
 
 @Component({
   selector: 'connect-webcam',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connect.component.css']
 })
 export class ConnectComponent implements OnInit {
-
+  
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
+  
+  public webcamImage: WebcamImage = null;
+  handleImage(webcamImage: WebcamImage) {
+    this.webcamImage = webcamImage;
+  }
+  
 }
