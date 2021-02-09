4fb16d8e6bc7eda7b7b796f49c4c0a50668c57c6
Adrian <16ar12@queensu.ca>
2021-02-09 16:45:28 -0500

    Update to service testing files to ensure functionality

--- src/app/services/camera.service.spec.ts
+++ src/app/services/camera.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { CameraService } from './camera.service';
 
 describe('CameraService', () => {
   let service: CameraService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule ]
+    });
     service = TestBed.inject(CameraService);
   });
 
