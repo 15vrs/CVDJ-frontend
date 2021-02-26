b4c91f5f7176ca6a25e1bbb5e0b113f6fb351323
Vivian So <vivianrachelso@gmail.com>
2021-02-26 14:49:13 -0500

    update redirect uri to use dynamic base url

--- src/app/create-room/create-room.component.ts
+++ src/app/create-room/create-room.component.ts
@@ -23,7 +23,7 @@ export class CreateRoomComponent implements OnInit {
     var urlData = {
       'client_id': 'ce5b366904544b58beb4a235b44ffc6c',
       'response_type': 'code',
-      'redirect_uri': 'http://localhost:4200/callback/', 
+      'redirect_uri': window.location.origin + '/callback/', 
       'scope': 'user-read-playback-state user-modify-playback-state streaming user-read-email user-read-private playlist-modify-private playlist-modify-public user-read-recently-played'
     }
     var url = 'https://accounts.spotify.com/authorize?' + this.encodeUrlData(urlData);
