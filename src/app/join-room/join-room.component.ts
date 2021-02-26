3db9eba7d3431ec66d88998a02b011a420fa9917
Vivian So <vivianrachelso@gmail.com>
2021-02-26 17:28:29 -0500

    [feat] add warning on empty room code input

--- src/app/join-room/join-room.component.ts
+++ src/app/join-room/join-room.component.ts
@@ -10,7 +10,8 @@ import { BackendService } from '../services/backend.service';
 })
 export class JoinRoomComponent implements OnInit {
 
-  error = false;
+  backendError = false;
+  inputError = false;
   loading = false;
 
   constructor(
@@ -21,22 +22,30 @@ export class JoinRoomComponent implements OnInit {
   }
 
   submit(roomId: string) {
-    this.loading = true;
-    this.backend.getJoinRoom(roomId)
-    .subscribe(
-      response => {
-        this.router.navigate(['/connect']);
-        this.backend.setRoomId(roomId);
-        this.backend.setUserId(response.body.userId);
-        this.backend.setPlaylistUri(response.body.playlistUri);
-        this.backend.setAccessToken(response.body.accessToken);
-      },
-      error => {
-        if (error.status >= 400) {
-          this.error = true;
+    // don't show spinner if input empty
+    if (!roomId){
+      this.inputError = true;
+      this.backendError = false;
+    } else {
+      this.loading = true;
+      this.backend.getJoinRoom(roomId)
+      .subscribe(
+        response => {
+          this.router.navigate(['/connect']);
+          this.backend.setRoomId(roomId);
+          this.backend.setUserId(response.body.userId);
+          this.backend.setPlaylistUri(response.body.playlistUri);
+          this.backend.setAccessToken(response.body.accessToken);
+        },
+        error => {
+          if (error.status >= 400) {
+            this.loading = false;
+            this.backendError = true;
+            this.inputError = false;
+          }
         }
-      }
-    );
+      );
+    }
   }
 
 }
