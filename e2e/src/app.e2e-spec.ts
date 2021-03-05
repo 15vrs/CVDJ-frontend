5e3dae5a6e5677c97a46c05e5948f6b39c352f2a
Vivian So <vivianrachelso@gmail.com>
2021-03-05 15:48:57 -0500

    wip func test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,19 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { Wiremock } from './wiremock';
 
-describe('workspace-project App', () => {
+describe('CVDJ App', () => {
   let page: AppPage;
+  let wiremock: Wiremock;
 
   beforeEach(() => {
     page = new AppPage();
+    wiremock = new Wiremock();
   });
 
-  it('should display welcome message', () => {
+  it('should display landing page', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('CVDJ app is running!');
+    expect(page.getTitleText()).toEqual('Computer Vision DJ');
   });
 
   afterEach(async () => {
